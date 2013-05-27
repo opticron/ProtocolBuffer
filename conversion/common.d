cfff74b9bcb45fde92a23551f6cd97463c1f38cb
Jesse Phillips <Jesse.K.Phillips@gmail.com>
2013-05-27 11:21:49 -0700

    Add docs for CodeBuilder

--- conversion/common.d
+++ conversion/common.d
@@ -24,12 +24,55 @@ string indented(int indentCount) {
 		return repeat("\t", indentCount);
 }
 
+/**
+ * Specifies how indentation should be applied to this line.
+ *
+ * open - specifies that this opens a block and should indent
+ * future lines.
+ *
+ * close - specifies that this closes a block, itself and
+ * future lines should indent one less.
+ *
+ * none - perform not modification to the indentation.
+ */
 enum Indent { none, open, close = 4 }
 
 struct Memory {
 	private CodeBuilder.Operation[][string] saved;
 }
 
+/**
+ * An output range that provides extended functionality for
+ * constructing a well formatted string of code.
+ *
+ * CodeBuilder has three main operations.
+ *
+ * $(UL
+ *    $(LI put - Applies string directly to desired output)
+ *    $(LI push - Stacks string for later)
+ *    $(LI build - Constructs a sequence of strings)
+ * )
+ *
+ * $(H1 put)
+ *
+ * The main operation for placing code into the buffer. This will sequentially
+ * place code into the buffer. Similar operations are provided for the other
+ * operations.
+ *
+ * $(D rawPut) will place the string without indentation added.
+ *
+ * One can place the current indentation level without code by calling put("");
+ *
+ * $(H1 push)
+ *
+ * This places code onto a stack. $(B pop) can be used to put the code into the
+ * buffer.
+ *
+ * $(H1 build)
+ *
+ * Building code in a sequence can be pushed onto the stack, or saved to be put
+ * into the buffer later.
+ */
 struct CodeBuilder {
 	struct Operation {
 		string text;
@@ -43,17 +86,33 @@ struct CodeBuilder {
 
 	int indentCount;
 
+	/**
+	 */
 	static CodeBuilder opCall(int indentedCount) {
 		CodeBuilder b;
 		b.indentCount = indentedCount;
 		return b;
 	}
 
-	void rawPut(string str, Indent indent = Indent.none) {
-		upper ~= str;
-		put(indent);
-	}
-
+	/**
+	 * Places str into the buffer.
+	 *
+	 * put("text", Indent.open); will indent "text" at the current level and
+	 * future code will be indented one level.
+	 *
+	 * put("other", Indent.close); will indent "other" at one less the current
+	 * indentation.
+	 *
+	 * put("}{", Indent.close | Indent.open) will indent "}{" at one less the
+	 * current indentation and continue indentation for future code.
+	 *
+	 * rawPut provides the same operations but does not include the current
+	 * indentation level.
+	 *
+	 * To place indentation but no other code use put("");
+	 *
+	 * To reduce indentation without inserting code use put(Indent.close);
+	 */
 	void put(string str, Indent indent = Indent.none) {
 		switch(str) {
 			case "":
@@ -70,28 +129,47 @@ struct CodeBuilder {
 		}
 	}
 
+	/// ditto
+	void rawPut(string str, Indent indent = Indent.none) {
+		upper ~= str;
+		put(indent);
+	}
+
+	/// ditto
 	void put(Indent indent) {
 		assert(!(indent & Indent.close & Indent.open), "No-op indent");
 		if(indent & Indent.close) indentCount--;
 		if(indent & Indent.open) indentCount++;
 	}
 
+	/**
+	 * Places str onto a stack that can latter be popped into
+	 * the current buffer.
+	 *
+	 * See put for specifics.
+	 */
 	void push(string str, Indent indent = Indent.close) {
 		lower ~= [Operation(str, indent)];
 	}
 
+	/// ditto
 	void push(Indent indent) {
 		lower ~= [Operation(null, indent)];
 	}
 
+	/// ditto
 	void rawPush(string str, Indent indent = Indent.close) {
 		lower ~= [Operation(str, indent, true)];
 	}
 
+	/// ditto
 	void rawPush() {
 		lower ~= [Operation(null, Indent.none, true)];
 	}
 
+	/**
+	 * Places the top stack item into the buffer.
+	 */
 	void pop() {
 		assert(!lower.empty(), "Can't pop empty buffer");
 
@@ -106,33 +184,52 @@ struct CodeBuilder {
 			assumeSafeAppend(lower);
 	}
 
+	/**
+	 * Construct a code string outside of the current buffer.
+	 *
+	 * Used to construct a code string in sequence, as apposed
+	 * to pushing the desired code in reverse (making it harder
+	 * to read).
+	 *
+	 * A build can also be saved with a name and later called.
+	 */
 	void build(string str, Indent indent = Indent.none) {
 		store ~= Operation(str, indent);
 	}
 
+	/// ditto
 	void build(Indent indent) {
 		store ~= Operation(null, indent);
 	}
 
+	/// ditto
 	void buildRaw(string str, Indent indent = Indent.none) {
 		store ~= Operation(str, indent, true);
 	}
 
+	/**
+	 * See push and put, performed on the current build.
+	 */
 	void pushBuild() {
 		lower ~= store;
 		store = null;
 	}
 
+	/// ditto
 	void pushBuild(string name) {
 		lower ~= saved[name];
 	}
 
+	/// ditto
 	void putBuild(string name) {
 		pushBuild(name);
 		pop();
 	}
 
 
+	/**
+	 * Stores the build to be called on later with $(B name).
+	 */
 	void saveBuild(string name) {
 		saved[name] = store;
 		store = null;
@@ -144,10 +241,16 @@ struct CodeBuilder {
 		return m;
 	}
 
+	/**
+	 * Adds the memory to this CodeBuilder
+	 */
 	void mem(Memory m) {
 		saved = m.saved;
 	}
 
+	/**
+	 * Returns the buffer, applying an code remaining on the stack.
+	 */
 	string finalize() {
 		while(!lower.empty())
 			pop();
